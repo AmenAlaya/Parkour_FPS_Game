4d72be4404b1afbb8665c5df74f27c326570f07e
AmenAlaya <86960856+AmenAlaya@users.noreply.github.com>
2022-10-09 19:37:09 +0100

    Version 1.0

--- Assets/MyScripts/Player_Movement.cs
+++ Assets/MyScripts/Player_Movement.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Rendering;
 
@@ -9,8 +10,18 @@ public class Player_Movement : MonoBehaviour
 {
     [Header("Movement")]
     private float _movementSpeed;
+
     [SerializeField] private float _walkSpeed;
     [SerializeField] private float _sprintSpeed;
+
+    [SerializeField]
+    private float _slideSpeed;
+
+    private float _desiredMoveSpeed;
+    private float _lastDesiredMoveSpeed;
+
+    [SerializeField] private float _speedIncMultiplier;
+    [SerializeField] private float _slopeIncMultiplier;
 
     [SerializeField] private float groundDrag;
 
@@ -19,37 +30,38 @@ public class Player_Movement : MonoBehaviour
     [SerializeField] private float _airMuliplier;
     private bool _canJump = true;
 
-
     [Header("Crouch Check")]
     [SerializeField] private float _crouchSpeed;
+
     [SerializeField] private float _crouchYScale;
     private float _startYScale;
 
     [Header("Slope Handling")]
     [SerializeField] private float _maxScopeAngle;
+
     private RaycastHit _slopeHit;
     private bool _isSloping;
 
-
     [Header("Keysinds")]
     private KeyCode _jumpKey = KeyCode.Space;
+
     private KeyCode _sprintKey = KeyCode.LeftShift;
-    private KeyCode _crouchKey = KeyCode.LeftControl;
+    private KeyCode _crouchKey = KeyCode.C;
 
     [Header("Ground Check")]
     [SerializeField] private float _playerHieght;
+
     [SerializeField] private LayerMask _whatIsGround;
     private bool _isGrounded = true;
-
 
     [SerializeField] private Transform _orientation;
 
     private float _horizantalInput;
     private float _verticalInput;
 
-    Vector3 _moveDir;
+    private Vector3 _moveDir;
 
-    Rigidbody _myRb;
+    private Rigidbody _myRb;
 
     private Movement_Sate _myState;
 
@@ -58,23 +70,25 @@ public class Player_Movement : MonoBehaviour
         WALKING,
         SPRINTING,
         CROUCHING,
+        SLIDING,
         AIR
     }
 
-    void Start()
+    public bool isSliding;
+
+    private void Start()
     {
         _myRb = GetComponent<Rigidbody>();
         _myRb.freezeRotation = true;
         _startYScale = transform.localScale.y;
-
     }
 
-    void FixedUpdate()
+    private void FixedUpdate()
     {
         Move_Player();
     }
 
-    void Update()
+    private void Update()
     {
         _isGrounded = Physics.Raycast(transform.position, Vector3.down, _playerHieght * .5f + .2f, _whatIsGround);
 
@@ -87,7 +101,7 @@ public class Player_Movement : MonoBehaviour
         _myRb.useGravity = !On_Slope();
     }
 
-    void Check_Ground(bool isGrounded)
+    private void Check_Ground(bool isGrounded)
     {
         if (isGrounded)
             _myRb.drag = groundDrag;
@@ -95,7 +109,7 @@ public class Player_Movement : MonoBehaviour
             _myRb.drag = 0;
     }
 
-    void My_Input()
+    private void My_Input()
     {
         _horizantalInput = Input.GetAxisRaw("Horizontal");
         _verticalInput = Input.GetAxisRaw("Vertical");
@@ -106,7 +120,6 @@ public class Player_Movement : MonoBehaviour
             Jump();
 
             Invoke(nameof(jump_Sate), _jumpCooldown);
-
         }
 
         if (Input.GetKeyDown(_crouchKey))
@@ -123,28 +136,79 @@ public class Player_Movement : MonoBehaviour
 
     private void Sate_Handeler()
     {
-        if (Input.GetKey(_crouchKey))
+        if (isSliding)
+        {
+            _myState = Movement_Sate.SLIDING;
+
+            if (On_Slope() && _myRb.velocity.y < .1f)
+            {
+                _desiredMoveSpeed = _slideSpeed;
+            }
+            else
+            {
+                _desiredMoveSpeed = _sprintSpeed;
+            }
+        }
+        else if (Input.GetKey(_crouchKey))
         {
             _myState = Movement_Sate.CROUCHING;
-            _movementSpeed = _crouchSpeed;
+            _desiredMoveSpeed = _crouchSpeed;
         }
         else if (_isGrounded && Input.GetKey(_sprintKey))
         {
             _myState = Movement_Sate.SPRINTING;
-            _movementSpeed = _sprintSpeed;
+            _desiredMoveSpeed = _sprintSpeed;
         }
         else if (_isGrounded)
         {
             _myState = Movement_Sate.WALKING;
-            _movementSpeed = _walkSpeed;
+            _desiredMoveSpeed = _walkSpeed;
         }
         else
         {
             _myState = Movement_Sate.AIR;
         }
+
+        if (Math.Abs(_desiredMoveSpeed - _lastDesiredMoveSpeed) > 4f && _movementSpeed != 0)
+        {
+            StopAllCoroutines();
+            StartCoroutine(Smoothly_Lerp_Move_Speed());
+        }
+        else
+        {
+            _movementSpeed = _desiredMoveSpeed;
+        }
+
+        _lastDesiredMoveSpeed = _desiredMoveSpeed;
     }
 
-    void Move_Player()
+    private IEnumerator Smoothly_Lerp_Move_Speed()
+    {
+        float time = 0;
+        float difference = Mathf.Abs(_desiredMoveSpeed - _movementSpeed);
+        float startValue = _movementSpeed;
+
+        while (time < difference)
+        {
+            _movementSpeed = Mathf.Lerp(startValue, _desiredMoveSpeed, time / difference);
+
+            if (On_Slope())
+            {
+                float slopeAngle = Vector3.Angle(Vector3.up, _slopeHit.normal);
+                float slopeAngleIncrease = 1 + (slopeAngle / 90);
+                time += Time.deltaTime * _speedIncMultiplier * _slopeIncMultiplier * slopeAngleIncrease;
+            }
+            else
+            {
+                time += Time.deltaTime * _speedIncMultiplier;
+            }
+
+            yield return null;
+        }
+        _movementSpeed = _desiredMoveSpeed;
+    }
+
+    private void Move_Player()
     {
         //calculate movement direction
         _moveDir = _orientation.forward * _verticalInput + _orientation.right * _horizantalInput;
@@ -153,7 +217,7 @@ public class Player_Movement : MonoBehaviour
 
         if (On_Slope() && !_isSloping)
         {
-            _myRb.AddForce(Get_Splope_Move_Direction() * _movementSpeed * 20, ForceMode.Force);
+            _myRb.AddForce(Get_Splope_Move_Direction(_moveDir) * _movementSpeed * 20, ForceMode.Force);
 
             if (_myRb.velocity.y > 0) _myRb.AddForce(Vector3.down * 80f, ForceMode.Force);
         }
@@ -164,7 +228,7 @@ public class Player_Movement : MonoBehaviour
             _myRb.AddForce(_moveDir * _movementSpeed * 10f * _airMuliplier, ForceMode.Force);
     }
 
-    void Speed_Control()
+    private void Speed_Control()
     {
         if (On_Slope() && !_isSloping)
         {
@@ -183,7 +247,7 @@ public class Player_Movement : MonoBehaviour
         }
     }
 
-    void Jump()
+    private void Jump()
     {
         _isSloping = true;
         //Reset velocity
@@ -192,13 +256,13 @@ public class Player_Movement : MonoBehaviour
         _myRb.AddForce(transform.up * _jumpForce, ForceMode.Impulse);
     }
 
-    void jump_Sate()
+    private void jump_Sate()
     {
         _isSloping = false;
         this._canJump = true;
     }
 
-    bool On_Slope()
+    public bool On_Slope()
     {
         if (Physics.Raycast(transform.position, Vector3.down, out _slopeHit, _playerHieght * .5f + .3f))
         {
@@ -209,9 +273,8 @@ public class Player_Movement : MonoBehaviour
         return false;
     }
 
-    Vector3 Get_Splope_Move_Direction()
+    public Vector3 Get_Splope_Move_Direction(Vector3 dir)
     {
-        return Vector3.ProjectOnPlane(_moveDir, _slopeHit.normal).normalized;
+        return Vector3.ProjectOnPlane(dir, _slopeHit.normal).normalized;
     }
-
 }
